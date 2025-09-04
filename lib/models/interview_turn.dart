ea5fa2f97313e10dee5eb573f1114a1a2cfd92a3
colrvia-a11y <colrvia@gmail.com>
2025-09-03 22:49:31 -0500

    Add interview session model and Firestore service

--- lib/models/interview_turn.dart
+++ lib/models/interview_turn.dart
@@ -3,4 +3,16 @@ class InterviewTurn {
   final String text;
   final bool isUser;
   InterviewTurn({required this.text, required this.isUser});
+
+  Map<String, dynamic> toJson() => {
+        'text': text,
+        'isUser': isUser,
+      };
+
+  factory InterviewTurn.fromJson(Map<String, dynamic> json) {
+    return InterviewTurn(
+      text: json['text'],
+      isUser: json['isUser'] ?? false,
+    );
+  }
 }
