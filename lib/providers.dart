eab00cfb0487a8c25e47330050c5a3e4fa4a555a
colrvia-a11y <colrvia@gmail.com>
2025-09-05 15:10:08 -0500

    feat: add palette providers

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,7 +1,21 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:color_canvas/services/firebase_service.dart';
+import 'package:color_canvas/firestore/firestore_data_schema.dart';
 
 final authProvider = StreamProvider<User?>((ref) {
   return FirebaseService.authStateChanges;
+});
+
+final userPalettesProvider = FutureProvider<List<UserPalette>>((ref) async {
+  final user = FirebaseService.currentUser;
+  if (user == null) return [];
+  return FirebaseService.getUserPalettes(user.uid);
+});
+
+final favoriteColorsProvider =
+    FutureProvider<List<Paint>>((ref) async {
+  final user = FirebaseService.currentUser;
+  if (user == null) return [];
+  return FirebaseService.getUserFavoriteColors(user.uid);
 });
